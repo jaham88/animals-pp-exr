352aa2d8518e37944c4a2b08a87bb02dafa22aef
Jan Hamrin <janhamrin88@gmail.com>
2024-12-04 10:01:32 +0100

    Add project files.

---
+++ animals pp exr/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animals_pp_exr
+{
+    internal class Animal
+    {
+        public string Name { get; set; }
+        public string AnimalType { get; set; }
+        public int Age { get; set; }
+        public bool IsVaccinated { get; set; }
+        public int Legs { get; set; }
+        public Animal(string name, string animaltype, int age, bool isvaccinated, int legs)
+        {
+            this.Name = name;
+            this.AnimalType = animaltype;
+            this.Age = age;
+            this.IsVaccinated = isvaccinated;
+            this.Legs = legs;
+        }
+    }
+}
---
+++ animals pp exr/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace animals_pp_exr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Animal> animalsList = new List<Animal>
+            {
+                new Animal("Kalle", "Dog", 5, true, 4),
+                new Animal("Kissen", "Cat", 3, false, 4),
+                new Animal("Skalet", "Turtle", 47, true, 4),
+                new Animal("Linus", "Dog", 9, true, 4),
+                new Animal("Zoro", "Cat", 13, false, 4),
+                new Animal("Birdie", "Bird", 2, false, 2),
+                new Animal("Pippi", "Bird", 2, false, 3),
+                new Animal("Herr Nilsson", "Monkey", 5, true, 2),
+                new Animal("Lilla Gubben", "Horse", 11, true, 4),
+                new Animal("Svarten", "Horse", 3, false, 4),
+                new Animal("Viten", "Horse", 1, true, 4)
+
+            };
+
+            var howManyDogs = animalsList.
+                Count(d => d.AnimalType == "Dog");
+
+            Console.WriteLine($"Det är så här många hundar i listan = {howManyDogs}");
+
+            Console.WriteLine("\n" +
+                "----------------------------------------------------------------------------------------" +
+                "\n");
+
+            var OldestAnimal = animalsList.
+                OrderBy(a => a.Age).
+                First();
+            Console.WriteLine($"Namn: {OldestAnimal.Name}" +
+                $"\nRas: {OldestAnimal.AnimalType}" +
+                $"\nÅlder: {OldestAnimal.Age}" +
+                $"\nÄr äldsta djuret just nu.");
+
+            Console.WriteLine("\n" +
+                "----------------------------------------------------------------------------------------" +
+                "\n");
+
+            var isVaccinated = animalsList.
+                Where(v => v.IsVaccinated == true).ToList();
+            foreach ( var animal in isVaccinated )
+                Console.WriteLine($"vaccinerade: " +
+                    $"{animal.Name} {animal.AnimalType} {animal.Age}");
+
+            Console.WriteLine("\n" +
+                "----------------------------------------------------------------------------------------" +
+                "\n");
+
+            var fourLegsOverAgeThree = animalsList.
+                Where(l => l.Legs == 4 && l.Age > 3).
+                ToList();
+            foreach ( var animal in isVaccinated )
+                Console.WriteLine($"{animal.Name} {animal.AnimalType} {animal.Age}");
+
+            Console.WriteLine("\n" +
+                "----------------------------------------------------------------------------------------" +
+                "\n");
+
+
+
+        }
+    }
+}
